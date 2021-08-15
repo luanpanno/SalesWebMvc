f436860c69fa72976ddd5fe7b994c4195221d914
Luan Panno <luanpanno@gmail.com>
2021-08-14 22:37:49 -0300

    feat: new entities

--- src/SalesWebMvc/Data/SalesWebMvcContext.cs
+++ src/SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -9,5 +9,7 @@ namespace SalesWebMvc.Data
         public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : base(options) { }
 
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- src/SalesWebMvc/Startup.cs
+++ src/SalesWebMvc/Startup.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SalesWebMvc.Data;
-using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc
