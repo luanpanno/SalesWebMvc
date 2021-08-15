4cffc47b6aa3cc9d55563ca918d7b81e42468a22
Luan Panno <luanpanno@gmail.com>
2021-08-15 01:37:24 -0300

    feat: seller details view

--- src/SalesWebMvc/Controllers/SellersController.cs
+++ src/SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
+using System;
 
 namespace SalesWebMvc.Controllers
 {
@@ -9,13 +11,20 @@ namespace SalesWebMvc.Controllers
     {
         private readonly ILogger<SellersController> _logger;
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-
-
-        public SellersController(ILogger<SellersController> logger, SellerService sellerService)
+        public SellersController(ILogger<SellersController> logger, SellerService sellerService, DepartmentService departmentService)
         {
             _logger = logger;
             _sellerService = sellerService;
+            _departmentService = departmentService;
+        }
+
+        private Seller GetSellerById(int? id)
+        {
+            var seller = _sellerService.FindById(id.Value);
+
+            return seller;
         }
 
         public IActionResult Index()
@@ -27,7 +36,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -37,6 +49,40 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var seller = GetSellerById(id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+
+            var seller = GetSellerById(id);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- src/SalesWebMvc/Services/SellerService.cs
+++ src/SalesWebMvc/Services/SellerService.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Collections.Generic;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -21,8 +22,20 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller)
         {
-            seller.Department = _context.Department.First();
             _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(x => x.Department).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+
+            _context.Seller.Remove(seller);
             _context.SaveChanges();
         }
     }
